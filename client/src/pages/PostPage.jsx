0d759ed782320eb29a6fc785d0d1631159a0a735
anushree <anushreev53@gmail.com>
2025-02-24 19:07:27 +0530

    Add CallToAction component to PostPage

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { Button } from 'flowbite-react';
 import { Spinner } from 'flowbite-react';
+import CallToAction from '../components/CallToAction';
 
 
 export default function PostPage() {
@@ -10,7 +11,7 @@ export default function PostPage() {
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
 
-  console.log(post);
+  //console.log(post);
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -54,6 +55,10 @@ export default function PostPage() {
     </div>
     <div className='p-3 max-w-2xl mx-auto w-full post-content' dangerouslySetInnerHTML={{__html: post && post.content}}>
 
+    </div>
+
+    <div className="max-w-4xl mx-auto w-full">
+        <CallToAction />
     </div>
   </main>;
 }
