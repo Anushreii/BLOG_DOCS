25d9ccb77944b309b1abefa38fc44e30cfc8610f
anushree <anushreev53@gmail.com>
2025-02-25 11:38:09 +0530

    Add Dashboard component to admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from '../components/DashProfile'
 import DashPosts from '../components/DashPosts'
 import DashUsers from '../components/DashUsers';
 import DashComments from '../components/DashComments'
+import DashboardComp from '../components/DashboardComp'
 
 export default function Dashboard() {
   const location = useLocation()
@@ -38,6 +39,12 @@ useEffect(()=>{
 
     {/* dash-commetns */}
     {tab === 'comments' && <DashComments></DashComments>}
+
+    {/* dash comp */}
+    {tab === 'dash' && <DashboardComp></DashboardComp>}
+     
+
+
     </div>
     
   );
