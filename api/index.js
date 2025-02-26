6f4a1f7b473379d80f5eee30e8cfea0797728262
anushree <anushreev53@gmail.com>
2025-02-26 11:34:30 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ import postRoutes from './routes/post.route.js'
 import cookieParser from 'cookie-parser';
 import uploadroutes from './routes/upload.route.js';
 import commentRoutes from './routes/comment.route.js';
-//import path from 'path';
+import path from 'path';
 
 dotenv.config();
 
@@ -30,7 +30,7 @@ mongoose.connect(process.env.MONGO)
    console.log(err);
 });
 
-//const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -44,13 +44,13 @@ app.listen(3000,()=>{
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
 app.use('/api/post', postRoutes);
-app.use("/api", uploadroutes);
+// app.use("/api", uploadroutes);
 app.use('/api/comment', commentRoutes);
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
-// app.get('*',(req, res) =>{
-//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-// })
+ app.get('*',(req, res) =>{
+   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+ })
 
 
 app.use((err, req, res, next)=>{
